24373a3eb09b5342d386fdf6011756ce57b086df
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-06-23 19:50:43 -0300

    scroll smooth in javascript

--- NLW/main.js
+++ NLW/main.js
@@ -1,23 +1,28 @@
 /*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function () {
+  element.addEventListener('click', function (event) {
+    event.preventDefault()
     nav.classList.toggle('show')
   })
 }
 
 /* quando clicar em um item do menu, esconder o menu */
+
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
   link.addEventListener('click', function () {
     nav.classList.remove('show')
+    scroolSmooth(link)
   })
 }
 
 /* mudar o header da página quando der scroll */
+
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
@@ -32,6 +37,7 @@ window.addEventListener('scroll', function () {
 })
 
 /* Testimonials carousel slider swiper */
+
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
@@ -42,6 +48,7 @@ const swiper = new Swiper('.swiper-container', {
 })
 
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
+
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
@@ -58,3 +65,14 @@ scrollReveal.reveal(
   `,
   { interval: 100 }
 )
+/*aula ao vivo */
+
+function scroolSmooth(link) {
+  console.log(link)
+  const sectionId = link.getAtribute('href')
+  document
+    .querySelector(sectionId)
+    .scrollIntoView({
+      behavior: 'smooth'
+    })
+}
